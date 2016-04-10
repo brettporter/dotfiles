afccd3237940bd9eb03dc7ed3d6ed2a27b4a459b
Brett Porter <brett@apache.org>
2016-04-10 21:24:41 +1000

    cleanup Git branches

---
+++ bin/clean_git_branches.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ref=`git rev-parse --abbrev-ref HEAD`
+
+if [ "$ref" != "master" -a "$ref" != "develop" ]; then
+  echo Must be on master/develop to clean branches
+  exit 1
+fi
+
+# Check for modifications
+git diff-index --cached --quiet HEAD -- && git diff --no-ext-diff --quiet --exit-code
+if [ "$?" != 0 ]; then
+  echo Repository has local modifications, not cleaning
+  exit 1
+fi
+
+echo Cleaning up
+
+git remote prune origin
+git branch -l --merged | grep -v develop | grep -v master | xargs git branch -d
