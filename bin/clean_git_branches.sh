02ccaf991733ce410517f7a55de9588b38f9fc61
Brett Porter <brett.porter@gmail.com>
2019-07-01 22:36:15 +1000

    remove squashed branches as well

--- bin/clean_git_branches.sh
+++ bin/clean_git_branches.sh
@@ -16,3 +16,11 @@ fi
 
 git remote prune origin
 git branch -l --merged | grep -v develop | grep -v master | xargs git branch -d
+
+# Check for squashed
+# Source: https://github.com/not-an-aardvark/git-delete-squashed
+git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch
+do
+	mergeBase=$(git merge-base master $branch) && [[ $(git cherry master $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch
+done
+
